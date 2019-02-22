b323a96631d34ad021f26ff38e3ed15915d15ebe
Aleksejs Popovs <aleksejs@popovs.lv>
2019-02-21 21:43:06 -0500

    add graphical ui

--- js/ui.js
+++ js/ui.js
@@ -1,5 +1,10 @@
 'use strict';
 
+var CANVAS_TILE_SIZE = 32;
+var CANVAS_TILES = {};
+
+var canvas_tiles_to_load = 0;
+
 function ui_state_to_text(state) {
     // (State) -> String
     var lines = [];
@@ -89,4 +94,45 @@ function ui_parse_map(map_text) {
     }
 
     return res;
+}
+
+function ui_make_image(src) {
+    canvas_tiles_to_load++;
+    var res = new Image();
+    res.src = src;
+    res.onload = function () {canvas_tiles_to_load--};
+    return res;
+}
+
+function ui_preload_canvas_tiles() {
+    CANVAS_TILES[TILE_EMPTY] = ui_make_image('./img/tile_empty.png');
+    CANVAS_TILES[TILE_FIXED] = ui_make_image('./img/tile_fixed.png');
+    CANVAS_TILES[TILE_BLOCK] = ui_make_image('./img/tile_block.png');
+    CANVAS_TILES[TILE_FINISH] = ui_make_image('./img/tile_finish.png');
+    CANVAS_TILES['player'] = ui_make_image('./img/tile_player.png');
+}
+
+function ui_resize_canvas(canvas, state) {
+    canvas.width = state.level.width * CANVAS_TILE_SIZE;
+    canvas.height = state.level.height * CANVAS_TILE_SIZE;
+}
+
+function ui_redraw_canvas(canvas, state) {
+    var ctx = canvas.getContext('2d');
+    ctx.clearRect(0, 0, CANVAS_TILE_SIZE * state.level.width, CANVAS_TILE_SIZE * state.level.height);
+
+    if (canvas_tiles_to_load > 0) {
+        ctx.fillText('please wait, loading tiles', 10, 30);
+        setTimeout(function () {ui_redraw_canvas(canvas, state)}, 100);
+        return;
+    }
+
+    for (var i = 0; i < state.level.height; ++i) {
+        for (var j = 0; j < state.level.width; ++j) {
+            ctx.drawImage(CANVAS_TILES[state.map[i][j]],
+                j * CANVAS_TILE_SIZE, i * CANVAS_TILE_SIZE);
+        }
+    }
+    ctx.drawImage(CANVAS_TILES['player'],
+        state.x * CANVAS_TILE_SIZE, state.y * CANVAS_TILE_SIZE);
 }
